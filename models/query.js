25297adec0d4d25e197344a95fbabb81df5841c5
Muruga <sgdms123@gmail.com>
2023-12-08 15:28:31 +0530

    models modified

--- models/query.js
+++ models/query.js
@@ -1,68 +1,81 @@
 import mysql from 'mysql';
 import 'dotenv/config';
 
-let con = mysql.createConnection({
-    host: process.env.MYSQL_HOST, //ENV_VARIABLES
-    user: process.env.MYSQL_USER, //ENV_VARIABLES
-    password: process.env.MYSQL_PASSWORD, //ENV_VARIABLES
-    database: process.env.MYSQL_DB //ENV_VARIABLES
-})
+// Create a connection pool
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST, // ENV_VARIABLES
+    user: process.env.MYSQL_USER, // ENV_VARIABLES
+    password: process.env.MYSQL_PASSWORD, // ENV_VARIABLES
+    database: process.env.MYSQL_DB, // ENV_VARIABLES
+});
 
 export const execute = (query) => {
     const promise = new Promise((resolve) => {
-        con.query(query, (err, result) => {
+        // Get a connection from the pool
+        pool.getConnection((err, connection) => {
             if (err) {
                 let e = {
                     error: JSON.stringify(err),
                     errorType: "MYSQL_ERROR",
-                    errorMessage: `Error in Query execution: ${query}`
-                }
-                resolve(e)
+                    errorMessage: `Error in getting connection: ${query}`
+                };
+                resolve(e);
             } else {
-                resolve(result)
+                // Execute the query
+                connection.query(query, (err, result) => {
+                    if (err) {
+                        let e = {
+                            error: JSON.stringify(err),
+                            errorType: "MYSQL_ERROR",
+                            errorMessage: `Error in Query execution: ${query}`
+                        };
+                        resolve(e);
+                    } else {
+                        resolve(result);
+                    }
+
+                    // Release the connection back to the pool
+                    connection.release();
+                });
             }
+        });
+    });
 
-            // End the connection after the query
-            con.end((endErr) => {
-                if (endErr) {
-                    console.error('Error ending connection:', endErr.stack);
-                } else {
-                    console.log('Connection ended');
-                }
-            });
-        })
-    })
-
-    return promise
-
+    return promise;
 };
-
 
 export const bulkUpdate = (query, values) => {
     const promise = new Promise((resolve) => {
-        
-        con.query(query, [values], (err, result) => {
+        // Get a connection from the pool
+        pool.getConnection((err, connection) => {
             if (err) {
                 let e = {
                     error: JSON.stringify(err),
                     errorType: "MYSQL_ERROR",
-                    errorMessage: `Error in Query execution: ${query}`
-                }
-                resolve(e)
+                    errorMessage: `Error in getting connection: ${query}`
+                };
+                resolve(e);
             } else {
-                resolve(result)
+                // Execute the query
+                connection.query(query, [values], (err, result) => {
+                    if (err) {
+                        let e = {
+                            error: JSON.stringify(err),
+                            errorType: "MYSQL_ERROR",
+                            errorMessage: `Error in Query execution: ${query}`
+                        };
+                        resolve(e);
+                    } else {
+                        resolve(result);
+                    }
+
+                    // Release the connection back to the pool
+                    connection.release();
+                });
             }
+        });
+    });
 
-            // End the connection after the query
-            con.end((endErr) => {
-                if (endErr) {
-                    console.error('Error ending connection:', endErr.stack);
-                } else {
-                    console.log('Connection ended');
-                }
-            });
-        })
-    })
-
-    return promise
+    return promise;
 };
